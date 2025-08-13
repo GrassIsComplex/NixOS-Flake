816304ee40ff0bbc9210f759cd65d2afd6a27d95
GrassIsComplex <eneandresioan@gmail.com>
2025-08-13 17:11:37 +0300

    Nvim blink.nvim plugin

--- home/config/nvim/lua/plugins/completion.lua
+++ home/config/nvim/lua/plugins/completion.lua
@@ -2,7 +2,6 @@ return {
   'saghen/blink.cmp',
   -- optional: provides snippets for the snippet source
   dependencies = { 'rafamadriz/friendly-snippets' },
-
   -- use a release tag to download pre-built binaries
   version = '1.*',
   -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
@@ -23,7 +22,7 @@ return {
     -- C-n/C-p or Up/Down: Select next/previous item
     -- C-e: Hide menu
     -- C-k: Toggle signature help (if signature.enabled = true)
-    --
+
     -- See :h blink-cmp-config-keymap for defining your own keymap
     keymap = { preset = 'default' },
 
@@ -34,7 +33,7 @@ return {
     },
 
     -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
+    completion = { documentation = { auto_show = true } },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
