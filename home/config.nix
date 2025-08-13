0098417dfc3326ee7f3e065e11864613a73fc0fb
GrassIsComplex <eneandresioan@gmail.com>
2025-08-13 16:06:34 +0300

    neovim

--- home/config.nix
+++ home/config.nix
@@ -5,6 +5,7 @@ in
 	home.file = {
 		"Scripts/theme-switcher.sh".source = "${config}/theme-switcher.sh";
 		".config/hypr".source = "${config}/hypr";
+		".config/nvim".source = "${config}/nvim";
 		".config/kitty".source = "${config}/kitty";
 		".config/waybar".source = "${config}/waybar";
 		".config/fastfetch".source = "${config}/fastfetch";
